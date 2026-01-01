8e118f4e12c83945990a839e2f9f82192031c263
guilhermyrs <developerguilhermy@gmail.com>
2026-01-01 17:22:09 -0300

    feat: adicionar guardião de autenticação JWT e decorador público para controle de acesso

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,16 +2,19 @@ import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignupDto } from "./dto/signup.dto";
 import { SigninDto } from "./dto/signin.dto";
+import { Public } from "../../common/decorators/public.decorator";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post("signup")
   signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
 
+  @Public()
   @Post("signin")
   signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
